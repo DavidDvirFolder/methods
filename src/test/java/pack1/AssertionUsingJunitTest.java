e85aaaab3393514b0e2533f1db70c518a2fbf914
dvirdd <dvird929@gmail.com>
2023-09-23 22:58:13 +0300

    add comments now

--- src/test/java/pack1/AssertionUsingJunitTest.java
+++ src/test/java/pack1/AssertionUsingJunitTest.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Urlssertion {
+public class AssertionUsingJunitTest {
     /*
     this is an example for url and title assertion using junit
 
